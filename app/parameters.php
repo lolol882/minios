a873f1b40aa740843ff9b68a32655b5025175fa0
lolol882 <lauraottdesign@gmail.com>
2016-03-24 16:11:53 +0100

    first

---
+++ app/parameters.php
@@ -0,0 +1,12 @@
+<?php
+
+$parameters = [
+    'domaine' => 'minios3/web/app.php',
+    'pdo' => [
+        'database_host'     => 'localhost',
+        'database_name'     => 'minios3',
+        'database_user'     => 'root',
+        'database_password' => '',
+    ],
+];
+
